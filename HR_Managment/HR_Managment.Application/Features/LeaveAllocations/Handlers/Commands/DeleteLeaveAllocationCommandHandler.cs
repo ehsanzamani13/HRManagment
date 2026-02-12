42d5c245ded4e608d76188c3cf81ad77dcda82ec
Ehsan Zamani <Ehsan Zamani>
2026-02-12 15:25:16 +0330

    Add DeleteLeaveTypeCommand Add DeleteLeaveAllocationCommand

---
+++ HR_Managment/HR_Managment.Application/Features/LeaveAllocations/Handlers/Commands/DeleteLeaveAllocationCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using HR_Managment.Application.Features.LeaveAllocations.Requests.Commands;
+using HR_Managment.Application.Persistence.Contracts;
+using MapsterMapper;
+using MediatR;
+
+namespace HR_Managment.Application.Features.LeaveAllocations.Handlers.Commands;
+
+public class DeleteLeaveAllocationCommandHandler :
+    IRequestHandler<DeleteLeaveAllocationCommand, Unit>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+
+    public DeleteLeaveAllocationCommandHandler(IMapper mapper, ILeaveAllocationRepository leaveAllocationRepository)
+    {
+        this._mapper = mapper;
+        this._leaveAllocationRepository = leaveAllocationRepository;
+    }
+    public async Task<Unit> Handle(DeleteLeaveAllocationCommand request, CancellationToken cancellationToken)
+    {
+        await _leaveAllocationRepository.Delete(request.Id);
+        return Unit.Value;
+    }
+}
---
+++ HR_Managment/HR_Managment.Application/Features/LeaveTypes/Handlers/Commands/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using HR_Managment.Application.Features.LeaveTypes.Requests.Commands;
+using HR_Managment.Application.Persistence.Contracts;
+using MapsterMapper;
+using MediatR;
+
+namespace HR_Managment.Application.Features.LeaveTypes.Handlers.Commands;
+
+public class DeleteLeaveTypeCommandHandler
+    : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly IMapper _mapper;
+
+    public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+    {
+        this._leaveTypeRepository = leaveTypeRepository;
+        this._mapper = mapper;
+    }
+    public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        await _leaveTypeRepository.Delete(request.Id);
+        return Unit.Value;
+    }
+}
