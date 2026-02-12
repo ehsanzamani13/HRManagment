b50e9dd2646b3c328414af5ad3da07ced0f5d17e
Ehsan Zamani <Ehsan Zamani>
2026-02-12 11:15:18 +0330

    Add GetLeaveAllocationList Request & Handler Add GetLeaveAllocationeDetail Request & Handler

---
+++ HR_Managment/HR_Managment.Application/Features/LeaveAllocations/Handlers/Queries/GetLeaveAllocationDetailRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using HR_Managment.Application.DTOs.LeaveAllocation;
+using HR_Managment.Application.Features.LeaveAllocations.Requests.Queries;
+using HR_Managment.Application.Persistence.Contracts;
+using MapsterMapper;
+using MediatR;
+
+namespace HR_Managment.Application.Features.LeaveAllocations.Handlers.Queries;
+
+public class GetLeaveAllocationDetailRequestHandler : IRequestHandler<GetLeaveAllocationDetailRequest, LeaveAllocationDto>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveAllocationRepository _allocationRepository;
+
+    public GetLeaveAllocationDetailRequestHandler(IMapper mapper , ILeaveAllocationRepository allocationRepository)
+    {
+        this._mapper = mapper;
+        this._allocationRepository = allocationRepository;
+    }
+    public async Task<LeaveAllocationDto> Handle(GetLeaveAllocationDetailRequest request, CancellationToken cancellationToken)
+    {
+        var leaveAllocation =  await _allocationRepository.Get(request.Id);
+        return _mapper.Map<LeaveAllocationDto>(leaveAllocation);
+    }
+}
---
+++ HR_Managment/HR_Managment.Application/Features/LeaveAllocations/Handlers/Queries/GetLeaveAllocationListRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using HR_Managment.Application.DTOs.LeaveAllocation;
+using HR_Managment.Application.Features.LeaveAllocations.Requests.Queries;
+using HR_Managment.Application.Persistence.Contracts;
+using MapsterMapper;
+using MediatR;
+
+namespace HR_Managment.Application.Features.LeaveAllocations.Handlers.Queries;
+
+public class GetLeaveAllocationListRequestHandler :
+    IRequestHandler<GetLeaveAllocationListRequest, List<LeaveAllocationDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+
+    public GetLeaveAllocationListRequestHandler(IMapper mapper, ILeaveAllocationRepository leaveAllocationRepository)
+    {
+        this._mapper = mapper;
+        this._leaveAllocationRepository = leaveAllocationRepository;
+    }
+    public async Task<List<LeaveAllocationDto>> Handle(GetLeaveAllocationListRequest request, CancellationToken cancellationToken)
+    {
+        var list = await _leaveAllocationRepository.GetAll();
+        return _mapper.Map<List<LeaveAllocationDto>>(list);
+    }
+}
